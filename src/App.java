e6f1a5bc70e9afe5d0d4d3b4a1765673a4216209
ZARASHIKI <agusyusup69@gmail.com>
2022-01-10 17:02:34 +0800

    baru

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import java.sql.SQLException;
 import java.sql.Statement;
-import com.mysql.cj.x.protobuf.MysqlxCrud.Update;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -10,8 +9,10 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.sql.ResultSet;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
 
 public class App extends Application {
+    TableView<makanan> tableView = new TableView<makanan>();
 
     public static void main(String[] args) {
         launch(args);
@@ -20,9 +21,6 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("UAS OOP");
-
-        TableView<makanan> tableView = new TableView<makanan>();
-
         TableColumn<makanan, String> columnID = new TableColumn<>("No");
         columnID.setCellValueFactory(new PropertyValueFactory<>("Id"));
 
@@ -42,9 +40,17 @@ public class App extends Application {
         toolBar.getItems().add(button1);
         button1.setOnAction(e -> add());
 
-        Button button2 = new Button("Delete ");
+        Button button2 = new Button("Delete");
         toolBar.getItems().add(button2);
-        button2.setOnAction(e -> add());
+        button2.setOnAction(e -> delete());
+
+        Button button3 = new Button("EDIT");
+        toolBar.getItems().add(button3);
+        button3.setOnAction(e -> edit());
+
+        Button button4 = new Button("Refresh");
+        toolBar.getItems().add(button4);
+        button4.setOnAction(e -> re());
 
         VBox vbox = new VBox(tableView, toolBar);
 
@@ -53,7 +59,7 @@ public class App extends Application {
         primaryStage.setScene(scene);
 
         primaryStage.show();
-
+        load();
         Statement stmt;
         try {
             Database db = new Database();
@@ -97,9 +103,9 @@ public class App extends Application {
                 sql = String.format(sql, namaField.getText(), hargaField.getText());
                 state.execute(sql);
                 addStage.close();
-
+                load();
             } catch (SQLException e1) {
-                // TODO Auto-generated catch block
+
                 e1.printStackTrace();
             }
         });
@@ -108,4 +114,119 @@ public class App extends Application {
         addStage.show();
     }
 
+    public void delete() {
+        Stage addStage = new Stage();
+        Button save = new Button("DELETE");
+
+        addStage.setTitle("Delete Data");
+
+        TextField noField = new TextField();
+        Label labelNo = new Label("Menu");
+
+        VBox hbox1 = new VBox(5, labelNo, noField);
+        VBox vbox = new VBox(20, hbox1, save);
+
+        Scene scene = new Scene(vbox, 400, 400);
+
+        save.setOnAction(e -> {
+            Database db = new Database();
+            try {
+                Statement state = db.conn.createStatement();
+                String sql = "delete from menu WHERE nama='%s'";
+                sql = String.format(sql, noField.getText());
+                state.execute(sql);
+                addStage.close();
+                load();
+            } catch (SQLException e1) {
+
+                e1.printStackTrace();
+                System.out.println();
+            }
+        });
+
+        addStage.setScene(scene);
+        addStage.show();
+    }
+
+    public void edit() {
+        Stage addStage = new Stage();
+        Button save = new Button("Simpan");
+
+        addStage.setTitle("Edit Menu");
+
+        TextField namaField = new TextField();
+        TextField hargaField = new TextField();
+        Label labelNama = new Label("Menu");
+        Label labelharga = new Label("Harga");
+
+        VBox hbox1 = new VBox(5, labelNama, namaField);
+        VBox hbox2 = new VBox(5, labelharga, hargaField);
+        VBox vbox = new VBox(20, hbox1, hbox2, save);
+
+        Scene scene = new Scene(vbox, 400, 400);
+
+        save.setOnAction(e -> {
+            Database db = new Database();
+            try {
+                Statement state = db.conn.createStatement();
+                String sql = "UPDATE menu SET Harga ='%s' WHERE nama='%s'";
+                sql = String.format(sql, hargaField.getText(), namaField.getText());
+                state.execute(sql);
+                addStage.close();
+                load();
+            } catch (SQLException e1) {
+
+                e1.printStackTrace();
+            }
+        });
+
+        addStage.setScene(scene);
+        addStage.show();
+    }
+
+    public void load() {
+        Statement stmt;
+        tableView.getItems().clear();
+        try {
+            Database db = new Database();
+            stmt = db.conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from menu");
+            while (rs.next()) {
+                tableView.getItems().addAll(new makanan(rs.getInt("Id"), rs.getString("Nama"), rs.getString("Harga")));
+            }
+            stmt.close();
+            db.conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void re() {
+        Database db = new Database();
+        try {
+            Statement state = db.conn.createStatement();
+            String sql = "ALTER TABLE menu DROP id";
+            sql = String.format(sql);
+            state.execute(sql);
+            re2();
+
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+            System.out.println();
+        }
+    }
+
+    public void re2() {
+        Database db = new Database();
+        try {
+            Statement state = db.conn.createStatement();
+            String sql = "ALTER TABLE menu ADD id INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST";
+            sql = String.format(sql);
+            state.execute(sql);
+            load();
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+            System.out.println();
+        }
+    }
 }
